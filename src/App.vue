7915795b7c2c6de1ba41b0cc3dab4877a35b9e88
Mathieu BOUQUE <mathieu.bouque@edu.univ-fcomte.fr>
2022-10-25 17:00:43 +0200

    entrée données supabase et liens pages

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
+<script setup lang="ts">
+</script>
+
+
 <template>
   <nav>
-    <h4 class="text-xl">
-      <Bars3Icon class="inline-block h-5 w-5 text-blue-500" />
-      menu (dans <code class="font-mono">/src/App.vue</code>)
-    </h4>
     <ul>
       <li>
         <router-link class="text-red-600 underline" to="/">
-          lien vers
-          <code class="font-mono">/src/pages/index.vue</code>
+          <img src="@/assets/TikTak.svg">
         </router-link>
+      </li>
+      <li>
+        <router-link class="text-red-600 underline" to="/montre">
+          <code class="">Ma montre</code>
+        </router-link>
+      </li>
+      <li>
+        A propos
+      </li>
+      <li>
+        <router-link class="text-red-600 underline" to="/compte">
+          <img src="@/assets/compteicon.svg">
+        </router-link>
+      </li>
+      <li>
+        <img src="@/assets/panier.svg">
       </li>
     </ul>
   </nav>
@@ -19,7 +34,3 @@
     <router-view class="m-2 border-2 p-2" />
   </Suspense>
 </template>
-
-<script setup lang="ts">
-import { Bars3Icon } from "@heroicons/vue/20/solid";
-</script>
