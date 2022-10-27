804e1eb4878d7966e72e9cdc54ac0e82ef376272
Mathieu BOUQUE <mathieu.bouque@edu.univ-fcomte.fr>
2022-10-27 15:47:00 +0200

    correction espace moncompte

--- src/pages/compte.vue
+++ src/pages/compte.vue
@@ -3,23 +3,29 @@ import { supabase, user } from '../supabase';
 
 </script>
 <template>
-    <section class="bg-yellow-200 dark:bg-slate-700 dark:text-gray-100 text-center pb-[50%]" >
-        <span class="sr-only">Connectez-vous</span>
-        <div>
-            <button class="text-xl font-aubrey bg-amber-500 dark:bg-gray-900 rounded-[25px] m-5" v-if="user" @pointerdown="supabase.auth.signOut()">
-                Se déconnecter ({{user.email}})
-            </button>
-            <button class="text-xl font-aubrey bg-amber-500 dark:bg-gray-900 rounded-[25px] m-5" v-else @pointerdown="supabase.auth.signIn({provider: 'facebook'})">
-                Se connecter avec Facebook
-            </button>
-        </div>
-        <div>
-            <button class="text-xl font-aubrey bg-amber-500 dark:bg-gray-900 rounded-[25px] m-5" v-if="user" @pointerdown="supabase.auth.signOut()">
-                Se déconnecter ({{user.email}})
-            </button>
-            <button class="text-xl font-aubrey bg-amber-500 dark:bg-gray-900 rounded-[25px] m-5" v-else @pointerdown="supabase.auth.signIn({provider: 'google'})">
-                Se connecter avec Google
-            </button>
-        </div>
-    </section>
+    <main class="-mb-20">
+        <section class="bg-yellow-200 dark:bg-slate-700 dark:text-gray-100 text-center pb-[50%]">
+            <span class="sr-only">Connectez-vous</span>
+            <div>
+                <button class="text-xl font-aubrey bg-amber-500 dark:bg-gray-900 rounded-[25px] m-5" v-if="user"
+                    @pointerdown="supabase.auth.signOut()">
+                    Se déconnecter ({{ user.email }})
+                </button>
+                <button class="text-xl font-aubrey bg-amber-500 dark:bg-gray-900 rounded-[25px] m-5" v-else
+                    @pointerdown="supabase.auth.signIn({ provider: 'facebook' })">
+                    Se connecter avec Facebook
+                </button>
+            </div>
+            <div>
+                <button class="text-xl font-aubrey bg-amber-500 dark:bg-gray-900 rounded-[25px] m-5" v-if="user"
+                    @pointerdown="supabase.auth.signOut()">
+                    Se déconnecter ({{ user.email }})
+                </button>
+                <button class="text-xl font-aubrey bg-amber-500 dark:bg-gray-900 rounded-[25px] m-5" v-else
+                    @pointerdown="supabase.auth.signIn({ provider: 'google' })">
+                    Se connecter avec Google
+                </button>
+            </div>
+        </section>
+    </main>
 </template>  
